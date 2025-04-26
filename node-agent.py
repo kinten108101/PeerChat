51b95c41109a305138592fdb92b8a048b4d70ea2
Kinten Le <kinten108101@protonmail.com>
2025-04-26 14:38:49 +0700

    fetch: promise api

--- node-agent.py
+++ node-agent.py
@@ -7,13 +7,13 @@ from lib.fetch import fetch
 def peer_connect(address):
   pass
 
-def submit_info():
+def work_submit_info():
   def on_response(response):
     print(f"agent: tracker responsed: \"{response}\"")
   body = {}
   body["address"] = ("127.0.0.1", 723)
   print(f"agent: submitting info to tracker")
-  fetch(TRACKER_ADDRESS, "submit_info", body, on_response)
+  return fetch(TRACKER_ADDRESS, "submit_info", body).then(on_response)
 
 if __name__ == "__main__":
   dotenv.source(prefix="node-agent")
@@ -23,4 +23,4 @@ if __name__ == "__main__":
   trackaddr = trackaddr.split(":")
   global TRACKER_ADDRESS
   TRACKER_ADDRESS = trackaddr[0], int(trackaddr[1])
-  submit_info()
+  work_submit_info().start()
