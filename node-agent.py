b53dc1b25555016e6954d60e3069493d621db633
Kinten Le <kinten108101@protonmail.com>
2025-04-27 10:14:14 +0700

    node-agent: mock listen

--- node-agent.py
+++ node-agent.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 from os import getenv
 import lib.dotenv as dotenv
+from lib.server import listen
 from lib.fetch import fetch
+from lib.cancellable import Cancellable
 
 NODE_ADDRESS = ("127.0.0.1", 7230)
 
@@ -16,6 +18,9 @@ def work_submit_info():
   print(f"agent: submitting info to tracker")
   return fetch(TRACKER_ADDRESS, "submit_info", body).then(on_response)
 
+def on_connection(request, response):
+  pass
+
 if __name__ == "__main__":
   dotenv.source(prefix="node-agent")
   trackaddr = getenv("TRACKER_ADDRESS")
@@ -25,3 +30,5 @@ if __name__ == "__main__":
   global TRACKER_ADDRESS
   TRACKER_ADDRESS = trackaddr[0], int(trackaddr[1])
   work_submit_info().start()
+  cancellable = Cancellable()
+  listen(TRACKER_ADDRESS, on_connection, cancellable)
